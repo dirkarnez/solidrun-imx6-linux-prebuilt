56ac1606c1cd5f06c2665c86175054ae82e465e7
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-24 18:34:47 +0800

    - update makelinux.sh

--- makelinux.sh
+++ makelinux.sh
@@ -10,7 +10,9 @@ function announce () {
 }
 
 set -e
-#  CC=arm-none-linux-gnueabihf-gcc
+
+# CC=arm-none-linux-gnueabihf-gcc
+# make mrproper
 
 export PATH="/opt/gcc-linaro-7.5.0-2019.12-i686_arm-linux-gnueabihf/bin:$PATH" && \
 arm-linux-gnueabihf-gcc --version && \
@@ -20,7 +22,7 @@ cd ./linux-fslc && \
 git checkout "3b4f1a2b7c57f198641c0a45e23fe7255a164daf" && \
 export CROSS_COMPILE="arm-linux-gnueabihf-" && \
 export ARCH="arm" && \
-announce "Building linux" && \ # make mrproper
+announce "Building linux" && \
 announce "make imx_v7_cbi_hb_defconfig" && \
 make imx_v7_cbi_hb_defconfig && \
 announce "make zImage dtbs modules" && \
