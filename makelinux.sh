2f6d9a56fbd46457d4087d5b7cfc839d96f5cc4f
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-24 18:28:10 +0800

    - update makelinux.sh

--- makelinux.sh
+++ makelinux.sh
@@ -10,37 +10,29 @@ function announce () {
 }
 
 set -e
-
-export PATH=/opt/gcc-linaro-7.5.0-2019.12-i686_arm-linux-gnueabihf/bin:$PATH
-
-/opt/gcc-linaro-7.5.0-2019.12-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc --version
-
 #  CC=arm-none-linux-gnueabihf-gcc
-cd /workspace
 
-git clone --branch solidrun-imx_4.9.x_1.0.0_ga https://github.com/SolidRun/linux-fslc.git
-
-cd ./linux-fslc
-
-git checkout "3b4f1a2b7c57f198641c0a45e23fe7255a164daf"
-
-
-export CROSS_COMPILE="arm-linux-gnueabihf-"
-export ARCH="arm"
-
-# export UBOOT_CONFIG="mx6cuboxi_defconfig"
-which cc
-announce "Building linux"
-# make mrproper
-make imx_v7_cbi_hb_defconfig
-make zImage dtbs modules
-announce "linux build appears to have been successful"
-announce "copying files"
-install -v -m644 -D ./arch/arm/zImage /dist/zImage
-install -v -m644 -D ./arch/arm/boot/dts/*.dtb /dist
-
+export PATH="/opt/gcc-linaro-7.5.0-2019.12-i686_arm-linux-gnueabihf/bin:$PATH" && \
+arm-linux-gnueabihf-gcc --version && \
+cd /workspace && \
+git clone --branch solidrun-imx_4.9.x_1.0.0_ga https://github.com/SolidRun/linux-fslc.git && \
+cd ./linux-fslc && \
+git checkout "3b4f1a2b7c57f198641c0a45e23fe7255a164daf" && \
+export CROSS_COMPILE="arm-linux-gnueabihf-" && \
+export ARCH="arm" && \
+announce "Building linux" && \ # make mrproper
+announce "make imx_v7_cbi_hb_defconfig" && \
+make imx_v7_cbi_hb_defconfig && \
+announce "make zImage dtbs modules" && \
+make zImage dtbs modules && \
+announce "linux build appears to have been successful" && \
+ls -R ./arch/arm && \
+announce "copying files" && \
+install -v -m644 -D ./arch/arm/boot/zImage /dist/zImage && \
+install -v -m644 -D ./arch/arm/boot/dts/*.dtb /dist && \
+announce "files copied"
 
 # for file in $(find source -type f -name *.py); do
 #     install -m 644 -D ${file} dest/${file#source/}
 # done
-announce "files copied"
+
